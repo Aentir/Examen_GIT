60f383ad6f37ebf718a3b6f18e9ecd6aac3bd252
JoseLuis <joseluislloretcazorla@gmail.com>
2021-01-14 10:18:46 +0100

    Se ha cambiado el return "true" de la línea 15 a "false"

--- src/model/GestionDatos.java
+++ src/model/GestionDatos.java
@@ -6,17 +6,17 @@ public class GestionDatos {
 
 	}
 
-	//TODO: Implementa una función para abrir ficheros
+	//TODO: Implementa una funciï¿½n para abrir ficheros
 	
-	//TODO: Implementa una función para cerrar ficheros
+	//TODO: Implementa una funciï¿½n para cerrar ficheros
 	
 	public boolean compararContenido (String fichero1, String fichero2){
-		//TODO: Implementa la función
-		return true;
+		//TODO: Implementa la funciï¿½n
+		return false;
 	}
 	
 	public int buscarPalabra (String fichero1, String palabra, boolean primera_aparicion){
-		//TODO: Implementa la función
+		//TODO: Implementa la funciï¿½n
 		return 1;
 	}	
 
